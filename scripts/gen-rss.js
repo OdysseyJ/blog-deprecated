4726d1ceb7d849c0a2150aa24404657e2ddae405
seongwoon jeong <ggogumalove@gmail.com>
2021-12-15 01:48:56 +0900

    Add new post test and change author names

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Aaron',
+    site_url: 'https://aaron.dev.io',
+    feed_url: 'https://aaron.dev.io/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
