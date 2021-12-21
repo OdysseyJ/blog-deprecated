6915752243b7f5a1dd8c3752a221ab5b8f46ee9e
seongwoon jeong <ggogumalove@gmail.com>
2021-12-22 01:31:39 +0900

    12/21 algorithm solved

--- playground.py
+++ playground.py
@@ -1,5 +1,2 @@
-while True:
-    a, b = map(int, input().split(" "))
-    if a==0 and b==0:
-        break
-    print("factor" if b > a and b%a == 0 else "multiple" if a > b and a%b == 0 else "neither")
+a, b = map(int, input().split(" "))
+print(a+b)
