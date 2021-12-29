416517bcf7c472a7e83568cbab3d2aecbebb30f6
seongwoon jeong <ggogumalove@gmail.com>
2021-12-29 18:54:07 +0900

    12-29

--- playground.py
+++ playground.py
@@ -1,29 +1,15 @@
 n = int(input())
-m = int(input())
-broken = []
-if m != 0:
-    broken = list(map(int, input().split(" ")))
+nums = [int(input()) for _ in range(n)]
 
-buttons = [True] * 10
-answer = 1000000000
-
-for b in broken:
-    buttons[b] = False
-
-def can_move_by_num(num):
-    for k in str(num):
-        if not buttons[int(k)]:
-            return False
-    return True
-
-
-# 0 <= N <= 500000
-# 가능한 채널 = 무한대
-# 검사해야하는 범위 = 0~1000000
-for i in range(1000001):
-    if can_move_by_num(i):
-        answer = min(answer, len(str(i)) + abs(i-n))
-
-answer = min(answer, abs(n - 100))
-
-print(answer)
+for num in nums:
+    stack = [1, 2, 3]
+    count = 0
+    while stack:
+        top = stack.pop()
+        if top == num:
+            count += 1
+        elif top < num:
+            stack.append(top+1)
+            stack.append(top+2)
+            stack.append(top+3)
+    print(count)
