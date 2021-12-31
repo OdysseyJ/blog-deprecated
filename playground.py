a85926723b3ed3a2ad3fc89de84be47552c11aa1
seongwoon jeong <ggogumalove@gmail.com>
2021-12-31 21:31:04 +0900

    12/31

--- playground.py
+++ playground.py
@@ -1,20 +1,6 @@
-n = int(input())
+from itertools import permutations
 
-#  if n <= 9:
-#      print(1*n)
-#  if n >= 10 and n <= 99:
-#      print((1*9)+(2*(n-9)))
-#  if n >= 100 and n <= 999:
-#      print((1*9)+(2*90)+(3*(n-99)))
-#  if n >= 1000 and n <= 9999:
-#      print((1*9)+(2*90)+(3*900)+(4*(n-999)))
+n, m = map(int, input().split(" "))
 
-length = len(str(n))
-sum_ = 0
-for i in range(1, length+1):
-    if i == length:
-        sum_ += i*(n-int('9'*(i-1) if i>1 else 0))
-    else:
-        sum_ += i*int('9'+'0'*(i-1))
-
-print(sum_)
+for p in list(permutations([str(i) for i in range(1, n+1)], m)):
+    print(" ".join(p))
