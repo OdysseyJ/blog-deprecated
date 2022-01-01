832f5ecdebe23c2ec5ec2781927e42fecbfb3a40
seongwoon jeong <ggogumalove@gmail.com>
2022-01-01 18:43:29 +0900

    1/1

--- playground.py
+++ playground.py
@@ -2,5 +2,7 @@ from itertools import permutations
 
 n, m = map(int, input().split(" "))
 
-for p in list(permutations([str(i) for i in range(1, n+1)], m)):
-    print(" ".join(p))
+permutation = permutations([str(i) for i in range(1, n+1)], m)
+
+for p in sorted(set(map(lambda x:" ".join(sorted(x)), list(permutation)))):
+    print(p)
