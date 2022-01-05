24c516cdad5788f90c8b5ebd62fdebff4feeecc4
Aaron <aaron@teamturing.com>
2022-01-05 21:03:12 +0900

    1/5-3

--- playground.py
+++ playground.py
@@ -1,17 +1,39 @@
 n = int(input())
+nums = [int(input()) for _ in range(n)]
+stack = []
+result = []
+history = []
+visited = [False]*(n+1)
 
-def is_vps(string):
-    stack = []
-    for s in string:
-        if s=="(":
-            stack.append(s)
-        if s==")":
-            if not stack:
-                return "NO"
-            else:
-                stack.pop()
-    return "YES" if len(stack) == 0 else "NO"
+before = 1
+for num in nums:
+    if num >= before:
+        while num != before:
+            if not visited[before]:
+                history.append("+")
+                stack.append(before)
+                visited[before] = True
+            before += 1
+        result.append(num)
+        history.append("+")
+        history.append("-")
+        visited[num] = True
+    elif num < before:
+        if stack and stack[-1] == num:
+            result.append(stack.pop())
+            history.append("-")
+        else:
+            break
 
-for _ in range(n):
-    string = input()
-    print(is_vps(string))
+    if num == n:
+        while stack:
+            result.append(stack.pop())
+            history.append("-")
+        break
+    before = num
+
+if nums == result:
+    for h in history:
+        print(h)
+else:
+    print("NO")
