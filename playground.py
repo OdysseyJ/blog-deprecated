6e1b475caab53d69ab6f0c73b9e38386a542bf18
Aaron <aaron@teamturing.com>
2022-01-06 19:23:46 +0900

    python buildin function 정리 끝

--- playground.py
+++ playground.py
@@ -1,26 +1,27 @@
-import sys
+n = int(input())
+queue = []
 
-string = sys.stdin.readline().strip()
-count = int(sys.stdin.readline().strip())
-
-left = [c for c in string]
-right = []
-
-for _ in range(count):
-    command = sys.stdin.readline().strip()
-    if command == "L":
-        if left:
-            right.append(left.pop())
+for i in range(n):
+    command = input()
+    if command == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    if command == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+    if command == "size":
+        print(len(queue))
+    if command == "empty":
+        print(1 if queue else 0)
         continue
-    if command == "D":
-        if right:
-            left.append(right.pop())
+    if command == "pop":
+        if queue:
+            print(queue.remove(0))
         continue
-    if command == "B":
-        if left:
-            left.pop()
-        continue
-    added = command.split(" ")[1]
-    left.append(added)
-
-print("".join(left+list(reversed(right))))
+    num = command.split(" ")[1]
+    print(num)
+    queue.append(num)
