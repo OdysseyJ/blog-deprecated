3773231ce46971ad558f37ccef05d624cf593e72
Aaron <aaron@teamturing.com>
2022-01-06 20:10:38 +0900

    1/6

--- playground.py
+++ playground.py
@@ -1,27 +1,33 @@
+import sys
+
 n = int(input())
 queue = []
 
 for i in range(n):
-    command = input()
+    command = sys.stdin.readline().strip()
     if command == "front":
         if queue:
             print(queue[0])
         else:
             print(-1)
+        continue
     if command == "back":
         if queue:
             print(queue[-1])
         else:
             print(-1)
+        continue
     if command == "size":
         print(len(queue))
+        continue
     if command == "empty":
-        print(1 if queue else 0)
+        print(0 if queue else 1)
         continue
     if command == "pop":
         if queue:
-            print(queue.remove(0))
+            print(queue.pop(0))
+        else:
+            print(-1)
         continue
     num = command.split(" ")[1]
-    print(num)
     queue.append(num)
