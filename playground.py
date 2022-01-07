5e9c15f27506f8d47102d59402f22c7a59bdb043
Aaron <aaron@teamturing.com>
2022-01-07 15:28:01 +0900

    1/7 - study effective python (ongoing)

--- playground.py
+++ playground.py
@@ -1,12 +1,48 @@
-n, k = map(int, input().split(" "))
+import collections
+import sys
+print("Hello"
+      "world")
 
-queue = [str(i) for i in range(1, n+1)]
-result = []
+deque = collections.deque()
+n = int(sys.stdin.readline().strip())
 
-while queue:
-    for i in range(k-1):
-        queue.append(queue.pop(0))
-    result.append(queue.pop(0))
+for i in range(n):
+    line = sys.stdin.readline().strip()
 
-answer = ", ".join(result)
-print('<{}>'.format(answer))
+    if line == "pop_front":
+        if deque:
+            print(deque.popleft())
+        else:
+            print(-1)
+        continue
+    if line == "pop_back":
+        if deque:
+            print(deque.pop())
+        else:
+            print(-1)
+        continue
+    if line == "size":
+        print(len(deque))
+        continue
+    if line == "empty":
+        print(0 if deque else 1)
+        continue
+    if line == "front":
+        if deque:
+            print(deque[0])
+        else:
+            print(-1)
+        continue
+    if line == "back":
+        if deque:
+            print(deque[-1])
+        else:
+            print(-1)
+        continue
+    command, num = line.split(" ")
+    if command == "push_front":
+        deque.appendleft(num)
+        continue
+    if command == "push_back":
+        deque.append(num)
+        continue
