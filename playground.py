962e13a5f2d8200688750130ca2a50a7a951c78d
Odysseyj <odysseyj@naver.com>
2022-01-12 22:21:12 +0900

    1/12

--- playground.py
+++ playground.py
@@ -1,12 +1,11 @@
-x = int(input())
+n = int(input())
+prices = [0]+[int(i) for i in input().split(" ")]
 
-dp = [0] * 1000001
+dp = [0] * 1001
+dp[1] = prices[1]
 
-for i in range(2, x+1):
-    dp[i] = dp[i-1]+1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2]+1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3]+1)
+for i in range(2, n+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j] + prices[j])
 
-print(dp[x])
+print(dp[n])
