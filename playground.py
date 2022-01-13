abae0615cb57a875f5ae823e335b4e2548fb3460
Odysseyj <odysseyj@naver.com>
2022-01-13 22:03:48 +0900

    1/13-ps

--- playground.py
+++ playground.py
@@ -1,17 +1,13 @@
 n = int(input())
 
-mod = 1000000000
+# dp[n][i] = i으로 끝나는 n자리 2친수
+dp = [[0 for _ in range(3)] for _ in range(91)]
 
-# dp[n][i] = n자리 숫자를 가지는 녀석들 중 마지막 수가 i로 끝나는 계단수의 개수
-dp = [[0 for _ in range(10)] for _ in range(101)]
+dp[1][0] = dp[2][1] = 0
+dp[1][1] = dp[2][0] = 1
 
-for i in range(1, 10):
-    dp[1][i] = 1
+for i in range(3, 91):
+    dp[i][0] = dp[i-1][0] + dp[i-1][1]
+    dp[i][1] = dp[i-1][0]
 
-for i in range(2, 101):
-    dp[i][0] = dp[i-1][1] % mod
-    for j in range(1, 9):
-        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod
-    dp[i][9] = dp[i-1][8] % mod
-
-print(sum(dp[n])%mod)
+print(sum(dp[n]))
