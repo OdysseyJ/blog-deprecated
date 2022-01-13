5f1490ab47371eae9721f04c4b4f961e30015944
Odysseyj <odysseyj@naver.com>
2022-01-13 21:53:06 +0900

    1/13

--- playground.py
+++ playground.py
@@ -1,18 +1,17 @@
-import sys
+n = int(input())
 
-mod = 1000000009
-t = int(input())
+mod = 1000000000
 
-# dp[n][i] = 합의 마지막 수가 i일때, [1,2,3]을 이용해 n을 만드는 방법
-dp = [[0 for _ in range(4)] for _ in range(100001)]
+# dp[n][i] = n자리 숫자를 가지는 녀석들 중 마지막 수가 i로 끝나는 계단수의 개수
+dp = [[0 for _ in range(10)] for _ in range(101)]
 
-dp[1][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1
+for i in range(1, 10):
+    dp[1][i] = 1
 
-for i in range(4, 100001):
-    dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % mod
-    dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % mod
-    dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % mod
+for i in range(2, 101):
+    dp[i][0] = dp[i-1][1] % mod
+    for j in range(1, 9):
+        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod
+    dp[i][9] = dp[i-1][8] % mod
 
-for i in range(t):
-    n = int(sys.stdin.readline().strip())
-    print(sum(dp[n])%mod)
+print(sum(dp[n])%mod)
