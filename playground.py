50ccd39194aa75d16b5e89b7227a0587e50511e1
Odysseyj <odysseyj@naver.com>
2022-01-14 18:00:40 +0900

    1/14-shell script

--- playground.py
+++ playground.py
@@ -1,13 +1,4 @@
 n = int(input())
+nums = [int(k) for k in input().split(" ")]
 
-# dp[n][i] = i으로 끝나는 n자리 2친수
-dp = [[0 for _ in range(3)] for _ in range(91)]
-
-dp[1][0] = dp[2][1] = 0
-dp[1][1] = dp[2][0] = 1
-
-for i in range(3, 91):
-    dp[i][0] = dp[i-1][0] + dp[i-1][1]
-    dp[i][1] = dp[i-1][0]
-
-print(sum(dp[n]))
+print(nums)
