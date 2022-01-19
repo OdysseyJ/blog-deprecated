69be63bfca88a55ceb81cd537125fc0fc16a13c8
Odysseyj <odysseyj@naver.com>
2022-01-19 22:36:53 +0900

    1/19

--- playground.py
+++ playground.py
@@ -1,13 +1,14 @@
-import sys
-
-n = int(sys.stdin.readline().strip())
+n = int(input())
 nums = [int(num) for num in input().split(" ")]
 
-dp = [1 for _ in range(n)]
+dp = [[0 for _ in range(2)] for _ in range(100001)]
+dp[0][0] = nums[0]
+dp[0][1] = nums[0]
 
+result = max(dp[0])
 for i in range(1, n):
-    for j in range(i):
-        if nums[j] > nums[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    dp[i][0] = max(0, dp[i-1][0]) + nums[i]
+    dp[i][1] = max(dp[i-1][1] + nums[i], dp[i-1][0])
+    result = max(dp[i][0], dp[i][1], result)
 
-print(max(dp))
+print(result)
