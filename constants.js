af3eb30e0b50ddd112ecd983c7870445879ce6a3
Odysseyj <odysseyj@naver.com>
2022-02-06 16:13:09 +0900

    Build 에러 수정

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const URLS = {"S3IMAGE":"https://seongwoon-blog.s3.ap-northeast-2.amazonaws.com/images/"}
--- pages/constants.js
+++
@@ -1,1 +0,0 @@
-export const s3ImagePath = 'https://seongwoon-blog.s3.ap-northeast-2.amazonaws.com/images/'
