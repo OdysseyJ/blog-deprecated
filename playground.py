ebaf0f2d068a9a15ff88d4e33fa0a7df4f4ce23e
Odysseyj <odysseyj@naver.com>
2022-02-11 16:15:27 +0900

    2/11-ps

--- playground.py
+++ playground.py
@@ -1,13 +1,4 @@
-def solve(a, b, c):
-    if b == 1:
-        return a
-    half = solve(a, int(b/2), c)
-    result = half * half % c
-    if b % 2 != 0:
-        result = result * a % c
-    return result
+n = int(input())
 
-
-a, b, c = map(int, input().split(" "))
-
-print(solve(a, b, c))
+for i in range(n):
+    print("".join(["*" for _ in range(n-i)]))
